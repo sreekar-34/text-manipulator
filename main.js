55a1317df9f570cf5e660befead1ccfc410dedf8
sreekar-34 <95081023+sreekar-34@users.noreply.github.com>
2023-04-05 16:53:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+nose_x = 0;
+nose_y = 0;
+rightWristX = 0;
+leftWristX = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -11,6 +17,12 @@ function setup(){
 
 function draw(){
     background("skyblue");
+    fill("orange");
+    stroke("black");
+    textSize(difference);
+    text("Sreekar",nose_x,nose_y);
+
+    document.getElementById("text_size").innerHTML = "Size of the Text will be " + difference + " px";
 }
 
 function modelLoaded(){
@@ -20,5 +32,12 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+        console.log("noseX = " + nose_x + " noseY = " + nose_y);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
     }
 }
